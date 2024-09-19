faeb15b984c6b150004079837662adc593d395ad
Brennan Batalla <brennan.batalla@gmail.com>
2024-09-19 13:43:38 -0600

    Add send and settings icons to ChatFooter component

--- src/components/AvaChat/AvaChatContainer/components/ChatFooter.tsx
+++ src/components/AvaChat/AvaChatContainer/components/ChatFooter.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import USER_AVATAR from '../../../../assets/myHeadshot.png';
+import { LuSendHorizonal } from 'react-icons/lu';
+import { GoGear } from 'react-icons/go';
 
 export const ChatFooter = () => {
   const contextOptions = ['Onboarding', 'Referral'];
@@ -17,18 +19,25 @@ export const ChatFooter = () => {
           onChange={(e) => setInput(e.target.value)}
         />
       </div>
-      <div className={'flex gap-4'}>
+      <div className={'flex gap-4 items-center'}>
         <p className={' text-gray-600'}>Context:</p>
         <select
           value={context}
           onChange={(e) => setContext(e.target.value)}
-          className={'outline-0 bg-gray-50'}>
+          className={'outline-0 bg-gray-50 p-1 rounded-md'}>
           {contextOptions.map((option) => (
             <option key={option} value={option}>
               {option}
             </option>
           ))}
         </select>
+        <span className={'flex-1'} />
+        <button className={'btn btn-sm btn-ghost px-1'}>
+          <GoGear className={'text-2xl'} />
+        </button>
+        <button className={'btn btn-sm btn-ghost px-1'} disabled={!input}>
+          <LuSendHorizonal className={'text-2xl'} />
+        </button>
       </div>
     </div>
   );
